35994cd1d77ef68402045f4347a2fef512b5a4b0
Krishm197 <krish.ubc.j@gmail.com>
2024-10-23 14:03:12 -0500

    Indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ class WasteVisionApp:
         	st.subheader("Upload Image or Provide Image URL")
         	source = st.radio("Select input source:", ["Upload Image", "Image URL"])
         	if source == "Upload Image":
-                uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+            	uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
                 if uploaded_file is not None:
                 	image = Image.open(uploaded_file)
                 	st.image(image, caption="Uploaded Image", use_column_width=True)
